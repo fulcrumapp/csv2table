200f0082958d84119252e26183d4a16c1fbb64df
Tony Gaeta <tony.gaeta@gmail.com>
2017-01-09 13:41:56 -0500

    Initial commit.

---
+++ background.js
@@ -0,0 +1,5 @@
+chrome.browserAction.onClicked.addListener(function(tab) {
+  // No tabs or host permissions needed!
+  console.log('Turning ' + tab.url + ' red!');
+  chrome.tabs.executeScript(null, {file: "content_script.js"});
+});
