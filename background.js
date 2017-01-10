51d23a8cdafbfc0e8c0e73d5f404ccc1f6f262f4
Tony Gaeta <tony.gaeta@gmail.com>
2017-01-09 22:15:59 -0500

    Remove :poop:

--- background.js
+++ background.js
@@ -1,5 +1,3 @@
 chrome.browserAction.onClicked.addListener(function(tab) {
-  // No tabs or host permissions needed!
-  console.log('Turning ' + tab.url + ' red!');
   chrome.tabs.executeScript(null, {file: "content_script.js"});
 });
