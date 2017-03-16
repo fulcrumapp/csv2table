90b771284cc56ef6c7386cfe14003f0c33c08806
Tony Gaeta <tony.gaeta@gmail.com>
2017-03-16 19:44:25 -0400

    Add fixed header.

--- build.sh
+++ build.sh
@@ -5,6 +5,7 @@ cp -r node_modules/bootstrap/dist/fonts .
 
 cat node_modules/bootstrap/dist/css/bootstrap.min.css > styles.css && \
 cat node_modules/datatables.net-bs/css/*.css >> styles.css && \
+cat node_modules/datatables.net-fixedheader-bs/css/*.css >> styles.css && \
 cat index.css >> styles.css && \
 sed -i _tmp -e 's/\.\.\/fonts\//chrome-extension:\/\/__MSG_@@extension_id__\/fonts\//g' styles.css && \
 rm styles.css_tmp
--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var csv = require('csv-parse');
 var $ = require('jquery');
 var dt = require('datatables.net')(window, $);
 require('datatables.net-bs')(window, $);
+require('datatables.net-fixedheader')(window, $);
 var htmlTable = require('helper-html-table');
 
 var content = document.getElementsByTagName('pre')[0].firstChild.data.replace(/\t+$/, '');
@@ -9,5 +10,5 @@ var content = document.getElementsByTagName('pre')[0].firstChild.data.replace(/\
 csv(content, (err, data) => {
   document.body.style.fontFamily = 'sans-serif';
   document.body.innerHTML = htmlTable({attr: 'cellspacing="0" class="table table-striped table-bordered"', thead: [data[0]], tbody: data.slice(1)});
-  $('table').DataTable({paging: false});
+  $('table').DataTable({paging: false, fixedHeader: true});
 });
