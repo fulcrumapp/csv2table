7b9fd063d7dcc65e76fa2de529a5958b45a18dc8
Tyler Johnston <tylerjohnst@gmail.com>
2017-03-16 19:00:30 -0400

    Remove redundant addClass

--- index.js
+++ index.js
@@ -10,5 +10,4 @@ csv(content, (err, data) => {
   document.body.style.fontFamily = 'sans-serif';
   document.body.innerHTML = htmlTable({attr: 'cellspacing="0" class="table table-striped table-bordered"', thead: [data[0]], tbody: data.slice(1)});
   $('table').DataTable({paging: false});
-  $('table').addClass('table table-striped table-bordered')
 });
